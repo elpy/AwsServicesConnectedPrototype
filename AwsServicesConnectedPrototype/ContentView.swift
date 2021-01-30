ad510cec35921601a5fde45fb9f42cdc8349d940
Anton Skvortsov <silmion@gmail.com>
2021-01-30 16:46:13 +0600

    Initial Commit

---
+++ AwsServicesConnectedPrototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwsServicesConnectedPrototype
+//
+//  Created by Антон Скворцов on 30.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
